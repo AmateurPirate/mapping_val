5ab4a619508c723309c1598d3719d834bf7bf09f
Gavin <48704991+AmateurPirate@users.noreply.github.com>
2021-01-15 09:42:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import math
+
+class helpers:
+    def __init__(self):
+        pass
+
+    def euc_dist(self, p1: List[int], p2: List[int]) -> int:
+        x1, y1, z1 = p1
+        x2, y2, z2 = p2
+        euclidian_distance = math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
+        return euclidian_distance
+
+class 
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main.py
