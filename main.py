14f1a5153892692a882da168ee0f06ddb75d6782
gavin <gavin.heidenreich@gmail.com>
2021-01-18 10:59:38 -0500

    changed git ignore and added vol

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class helpers:
     def __init__(self):
         pass
 
-    def euc_dist(self, p1: List[int], p2: List[int]) -> int:
+    def euc_dist(self, p1, p2):
         x1, y1, z1 = p1
         x2, y2, z2 = p2
         euclidian_distance = math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
@@ -14,13 +14,21 @@ class helpers:
 class parse_csv:
     def __init__(self, csv_path):
         self.df = pd.read_csv(csv_path)
+        self.add_vol()
         self.get_six_points()
 
+    def add_vol(self):
+        for ch in 'xyz':
+            self.df[ch + 'vol'] = self.df['DLC_3D']['one'][ch].rolling(45).std()
+
     def get_six_points(self):
-        self.df['vol'] = pd.rolling_std(self.df, 45)
+        pass
 
 def main():
+    csv_path = './calib_ring_5_DLC_3D.csv' 
+    pc = parse_csv(csv_path)
+    print(pc.df.head())
     pass
 
 if __name__ == '__main__':
-    main.py
+    main()
